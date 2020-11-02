1558c340c27159be7db51a478c7c9100ddb0384b
Hayden Sansum <hayden.e.sansum@gmail.com>
2020-11-01 23:26:16 -0500

    adding second webpage required to access by ?frame

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, send_from_directory
+from flask import Flask, render_template, url_for, send_from_directory, request
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -6,8 +6,16 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-
+    query_string = request.query_string
+    if query_string is not None:
+        print(query_string)
+        if query_string == b'frame':
+            print('hi')
+            return render_template('static_page.html')
+        else:
+            return render_template('home.html')
+    else:
+        return render_template('home.html')
 
 @app.route('/static/<path:path>')
 def send_static(path):
@@ -15,6 +23,10 @@ def send_static(path):
 
 @app.route('/assets/<path:path>')
 def send_talavera(path):
+    return send_from_directory('assets', path)
+
+@app.route('/assets/<path:path>')
+def send_images(path):
     return send_from_directory('assets', path)
 
 if __name__ == '__main__':
